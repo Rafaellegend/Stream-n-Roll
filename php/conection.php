6fe1cfcca0b4101503f5688f188b1f96331b6249
Desalmadinho <52262209+Desalmadinho@users.noreply.github.com>
2021-02-17 12:55:42 -0300

    Tentativa de verificaação de conexão.
    Falhou, mantido a mesma de antes

--- php/conection.php
+++ php/conection.php
@@ -1,5 +1,6 @@
 <?php
 
+	
 	//Criando conexão com o Banco de Dados e Verificando a conexão
 	$hostname="localhost";
 	$username="root";
@@ -10,4 +11,22 @@
 	("Erro ao conectar ao bando de dados");
 	mysqli_select_db($dbname);
 	
+	
+	
+	/*
+	//Criando conexão com o Banco de Dados e Verificando a conexão
+	$hostname="localhost";
+	$username="root";
+	$password="usbw";
+	$dbname="StreamNRoll";
+	
+	$conn = mysqli_connect($hostname,$username,$password); 
+	mysqli_select_db($dbname) or die( "Não foi possível conectar ao banco MySQL");
+	if (!$conn) {echo "Não foi possível conectar ao banco MySQL.
+	"; exit;}
+	else {echo "Parabéns!! A conexão ao banco de dados ocorreu normalmente!.
+	";}
+	mysqli_close();
+	
+	*/
 ?>
